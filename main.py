375b9e720c202d758fd41e59c7beccf76de27756
Dominik Szczepański <163560221+Dominik-developer@users.noreply.github.com>
2024-09-06 22:10:07 +0200

    first function

--- main.py
+++ main.py
@@ -10,12 +10,54 @@ def clear_screen():
         os.system('clear')
 
     #egzample variables
-    a = 120000
+    '''a = 120000
     n=20000
-    i=0
+    i=0'''
 
+# logic functions
 
 def toNumber():
+
+    try:
+        endMoney = float(input("How much money you want to have at the end? "))
+        
+        try:
+            startMoney = float(input("How much money you have on start?"))
+
+            try:
+                percent = float(input("What is the interest percent?"))
+
+                # convertion of variables 
+                currentMoney = startMoney
+
+                interesPercent = percent + 1 
+
+                while( currentMoney < endMoney):
+                    
+
+                    currentMoney = currentMoney * interesPercent
+                    years = years + 1
+
+                    '''
+                    n = n*1.04
+                    n = n+20000
+                    i = i+1
+                    print(n)
+                    '''
+
+                    print(f"You will achive {endMoney} in {years}, at the end you will have {currentMoney}")
+
+            except ValueError:
+                print("Wrong data type in, try again!")
+
+        except ValueError:
+            print("Wrong data type in, try again!")
+
+    except ValueError:
+        print("Wrong data type in, try again!")
+
+ 
+
     while(n < 79000000):
 
         n = n*1.04
@@ -26,6 +68,15 @@ def toNumber():
     
 '''
 def fromNumber():
+
+    try:
+        number = float(input(" "))
+
+
+        print(f"Masz {number} lat.")
+    except ValueError:
+        print("Wrong data type in, try again!")
+
     for i in range(2):
         a= a *1.06 
         a = a + 0
@@ -39,4 +90,5 @@ clear_screen()
 print('compound interest calculator in python')
 print('Choose which type of calculations you would like to do:')
 
-toNumber()
+
+
