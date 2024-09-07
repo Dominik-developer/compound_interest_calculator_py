8ffe969495bad57a25e021a8fbc47e1dac6cc93f
Dominik Szczepański <163560221+Dominik-developer@users.noreply.github.com>
2024-09-07 16:14:27 +0200

    Finish toNumber function

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def clear_screen():
     n=20000
     i=0'''
 
+
 # logic functions
 
 def toNumber():
@@ -22,30 +23,28 @@ def toNumber():
         endMoney = float(input("How much money you want to have at the end? "))
         
         try:
-            startMoney = float(input("How much money you have on start?"))
+            startMoney = float(input("How much money you have on start? "))
 
             try:
-                percent = float(input("What is the interest percent?"))
+                percent = float(input("What is the interest percent? "))
 
                 # convertion of variables 
                 currentMoney = startMoney
 
-                interesPercent = percent + 1 
+                interesPercent = percent / 100 + 1
 
+                #algorithm
                 while( currentMoney < endMoney):
-                    
 
+                    years = 0
+                    
                     currentMoney = currentMoney * interesPercent
                     years = years + 1
 
-                    '''
-                    n = n*1.04
-                    n = n+20000
-                    i = i+1
-                    print(n)
-                    '''
-
-                    print(f"You will achive {endMoney} in {years}, at the end you will have {currentMoney}")
+                #rounding numbers
+                roundMoney = round(currentMoney, 2)
+            
+                print(f"You will exceed {endMoney} in {years} year/years, at the end you will have {roundMoney}. \n")
 
             except ValueError:
                 print("Wrong data type in, try again!")
@@ -56,39 +55,12 @@ def toNumber():
     except ValueError:
         print("Wrong data type in, try again!")
 
- 
 
-    while(n < 79000000):
+print('compound interest calculator in python\n')
+'''print('Choose which type of calculations you would like to do: ')'''
+print()
+print('--------------------\n')
 
-        n = n*1.04
-        n = n+20000
-        i = i+1
-        print(n)
-    
-    
-'''
-def fromNumber():
-
-    try:
-        number = float(input(" "))
-
-
-        print(f"Masz {number} lat.")
-    except ValueError:
-        print("Wrong data type in, try again!")
-
-    for i in range(2):
-        a= a *1.06 
-        a = a + 0
-    print(a)
-'''
-
-
-
-clear_screen()
-
-print('compound interest calculator in python')
-print('Choose which type of calculations you would like to do:')
-
+toNumber()
 
 
